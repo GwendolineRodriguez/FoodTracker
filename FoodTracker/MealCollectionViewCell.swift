e736d3d1a98ddcd39ef2cb80a468d46f7c05c7fa
Gwendoline Rodriguez <gwendolinerodriguez@gmail.com>
2017-10-18 14:00:13 +1000

    added collectionView

---
+++ FoodTracker/MealCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MealCollectionViewCell.swift
+//  FoodTracker
+//
+//  Created by Gwendoline Rodriguez on 18/10/17.
+//  Copyright © 2017 Godwin Apps. All rights reserved.
+//
+
+import UIKit
+
+class MealCollectionViewCell: UICollectionViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+}
