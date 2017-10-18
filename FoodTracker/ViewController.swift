8889b13e2ff757c3cf2a02cb972de20ce37c03fb
Gwendoline Rodriguez <gwendolinerodriguez@gmail.com>
2017-10-18 10:17:41 +1000

    finished implement a custom control

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -67,10 +68,6 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     
     //MARK: Actions
-    @IBAction func setDefaultLabelText(_ sender: UIButton) {
-        mealNameLabel.text = "Nouilles chinoises"
-    }
-
     @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
             // Hide the keyboard.
             nameTextField.resignFirstResponder()
